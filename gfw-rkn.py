6a3d23b58897ca458b33f960751ac7e66f9abb9f
okamietovolk <ok@mietovolk.ru>
2023-08-08 20:15:36 +0700

    feat: генерация конфига под shadowrocket.

--- gfw-rkn.py
+++ gfw-rkn.py
@@ -3,12 +3,14 @@ import requests
 import base64
 
 print('1 - сгенерировать конфиг GFW, 2 - сгенерировать конфиг Shadowrocket')
-choice = input()
+choice = int(input())
 blocklist = requests.get("https://reestr.rublacklist.net/api/v3/ct-domains/")
 blocklist = blocklist.json()
 
-if choice == 1:
-    with open('fckrkn.txt', 'w') as f:
+# Генерация конфига GFW
+
+if choice == 1:    
+    with open('fckrkn_GFW.txt', 'w') as f:
         f.write('!---------403/451/503/520 & URL Redirects---------')
         f.write('\n')
         f.write('! HomePage: https://github.com/okamietovolk/gfw-rkn')
@@ -22,15 +24,18 @@ if choice == 1:
         f.write('\n')
         f.write('!---------------------EOF-----------------------')
 
-    with open('fckrkn.txt', 'r') as f:
+#base64
+
+    with open('fckrkn_GFW.txt', 'r') as f:
         content = f.read()
 
     encoded_content = base64.b64encode(content.encode()).decode()
-elif choice == 2:
-    with open('fckrkn.txt', 'w') as f:
+    with open('fckrkn_GFW.txt', 'w') as f:
         f.write(encoded_content)
 
-        with open('fckrkn.txt', 'w') as f:
+# Генерация конфига shadowrocket
+elif choice == 2:
+        with open('fckrkn_shadowrocket.conf', 'w') as f:
             f.write('[General]\nprivate-ip-answer = true\nalways-real-ip = \nbypass-system = true\nskip-proxy = 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, localhost, *.local, captive.apple.com\ntun-excluded-routes = 10.0.0.0/8, 100.64.0.0/10, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, 192.0.2.0/24, 192.88.99.0/24, 192.168.0.0/16, 198.18.0.0/15, 198.51.100.0/24, 203.0.113.0/24, 224.0.0.0/4, 255.255.255.255/32\ndns-server = 8.8.8.8, 1.1.1.1/nipv6 = false\n\n[Rule]')
             for site in blocklist:
                 line = "DOMAIN-KEYWORD," + site + ",PROXY"
